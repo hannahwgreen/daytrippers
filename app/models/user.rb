ebc5e511064b8aab4019f245678349dd6302b08f
M. Del Zotto <m.delzotto@Ms-MacBook-Pro.local>
2018-03-13 13:21:27 -0400

    review table and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  mount_uploader :display_name, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         
 end
