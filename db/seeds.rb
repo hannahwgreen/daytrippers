96e18ac211f5ce32fe6e29b809ec089fdab4a0af
Hannah <hannahwgreen1@gmail.com>
2018-03-22 17:57:06 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Trip.destroy_all
+TripCategorizations.destroy_all
+Review.destroy_all
+Category.destroy_all
+
 
 u1 = User.create( display_name: 'Joe Schmoe', email: 'js@mailboys.com', password: 'phillyphilly' )
 u1.confirm
