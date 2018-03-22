f2a820f488015ab593f05021e94db3f30d5ef6db
Brett Holland <brett.holland@gmail.com>
2018-03-21 21:44:41 -0400

    More tests

---
+++ spec/features/admin_edits_user_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'admin edits user', %Q{
+  As an admin
+  I want to edit a user
+  So that it shows something different
+} do
+  let!(:user) { FactoryBot.create(:random_user) }
+  let!(:admin) { FactoryBot.create(:random_admin) }
+  
+  scenario 'edit an existing user' do
+    admin.confirm
+    visit new_user_session_path
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Log in'
+    
+    visit edit_user_path(user)
+    
+    fill_in 'Display name', with: 'Joel Embiid'
+    
+    click_button 'Update'
+    
+    expect(page).to have_content('Joel Embiid')
+  end  
+  
+  scenario 'try to edit a user without being an admin' do
+    user.confirm
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    
+    visit edit_user_path(user)
+        
+    expect(page).to have_content('You need permission to see this page.')
+  end
+end
